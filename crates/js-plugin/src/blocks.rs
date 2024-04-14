6fa86d560f3722d5912d380b4281452b6163eb14
Rioni <nrosa01@ucm.es>
2024-04-14 17:35:05 +0200

    Changed enum variants from tuples to structs

--- crates/js-plugin/src/blocks.rs
+++ crates/js-plugin/src/blocks.rs
@@ -14,22 +14,22 @@ type NumberLiteral = usize;
 #[derive(Debug,Serialize,Deserialize)]
 #[serde(tag = "block", content = "data", rename_all = "camelCase")]
 pub enum Blocks {
-    Particle(ParticleType), // Particle type
-    Swap{ direction: Direction },
-    CopyTo(Direction),
-    ChanteInto(Direction, ParticleType),
-    IfDirectionIsType{direction: Direction, r#type: ParticleType}, // If particle at direction is of type X
-    Not(Box<Blocks>),           // Negates a block result, it's inverting a boolean
-    And(Box<Blocks>, Box<Blocks>),   // Logical AND
-    Or(Box<Blocks>, Box<Blocks>),    // Logical OR
-    Touching(ParticleType),          // Looks neighbour to see if it's of type X
-    NumberOfXTouching(ParticleType), // Counts number of neighbours of type X
-    TypeOf(Direction),     // Returns type of particle at direction
-    If{condition: Box<Blocks>, result: Action},    // If block, if true, execute action
-    OneInXChance(NumberLiteral), // Returns true one in a X chance, for example, if X is 3, it will return true 1/3 of the time
-    CompareIs(Box<Blocks>, Box<Blocks>), // Compares two blocks
-    CompareBiggerThan(Box<Blocks>, Box<Blocks>), // Compares two blocks
-    CompareLessThan(Box<Blocks>, Box<Blocks>), // Compares two blocks
-    Direction(Direction), // Returns a direction
-    Boolean(bool),       // Returns a boolean value
+    Particle { r#type: ParticleType }, // Particle type
+    Swap { direction: Direction },
+    CopyTo { direction: Direction },
+    ChangeInto { direction: Direction, r#type: ParticleType },
+    IfDirectionIsType { direction: Direction, r#type: ParticleType }, // If particle at direction is of type X
+    Not { block: Box<Blocks> },           // Negates a block result, it's inverting a boolean
+    And { block1: Box<Blocks>, block2: Box<Blocks> },   // Logical AND
+    Or { block1: Box<Blocks>, block2: Box<Blocks> },    // Logical OR
+    Touching { r#type: ParticleType },          // Looks neighbour to see if it's of type X
+    NumberOfXTouching { r#type: ParticleType }, // Counts number of neighbours of type X
+    TypeOf { direction: Direction },     // Returns type of particle at direction
+    If { condition: Box<Blocks>, result: Action },    // If block, if true, execute action
+    OneInXChance { chance: NumberLiteral }, // Returns true one in a X chance, for example, if X is 3, it will return true 1/3 of the time
+    CompareIs { block1: Box<Blocks>, block2: Box<Blocks> }, // Compares two blocks
+    CompareBiggerThan { block1: Box<Blocks>, block2: Box<Blocks> }, // Compares two blocks
+    CompareLessThan { block1: Box<Blocks>, block2: Box<Blocks> }, // Compares two blocks
+    Direction { direction: Direction }, // Returns a direction
+    Boolean { value: bool },       // Returns a boolean value
 }
