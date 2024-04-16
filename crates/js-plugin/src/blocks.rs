bfb6767b0319e8e94762e78000ead9b2335c8ec2
Rioni <nrosa01@ucm.es>
2024-04-17 01:44:11 +0200

    Directions now support operations too

--- crates/js-plugin/src/blocks.rs
+++ crates/js-plugin/src/blocks.rs
@@ -13,6 +13,7 @@ type NumberLiteral = usize;
 pub enum Direction {
     Constant([i32; 2]),
     Random,
+    Operation(MathOperations, Box<Direction>, Box<Direction>),
 }
 
 impl Direction {
@@ -20,6 +21,21 @@ impl Direction {
         match self {
             Direction::Constant(direction) => *direction,
             Direction::Random => [api.gen_range(-1, 1), api.gen_range(-1, 1)],
+            Direction::Operation(op, dir1, dir2) => {
+                let dir1 = dir1.get_direction(api);
+                let dir2 = dir2.get_direction(api);
+                match op {
+                    MathOperations::Addition => [dir1[0] + dir2[0], dir1[1] + dir2[1]],
+                    MathOperations::Subtraction => [dir1[0] - dir2[0], dir1[1] - dir2[1]],
+                    MathOperations::Multiplication => [dir1[0] * dir2[0], dir1[1] * dir2[1]],
+                    MathOperations::Division => [dir1[0] / dir2[0], dir1[1] / dir2[1]],
+                    MathOperations::Modulo => [dir1[0] % dir2[0], dir1[1] % dir2[1]],
+                    MathOperations::Difference => [
+                        (dir1[0] - dir2[0]).abs(),
+                        (dir1[1] - dir2[1]).abs(),
+                    ],
+                }
+            }
         }
     }
 }
