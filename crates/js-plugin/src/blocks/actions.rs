d7ea2dce77c66df9eaeea0f021307893d1aced7f
Rioni <nrosa01@ucm.es>
2024-05-01 05:24:45 +0200

    Actions are now vectors

--- crates/js-plugin/src/blocks/actions.rs
+++ crates/js-plugin/src/blocks/actions.rs
@@ -8,14 +8,14 @@ pub enum Actions
     Swap { direction: Direction },
     CopyTo { direction: Direction },
     ChangeInto { direction: Direction, r#type: ParticleType },
-    RandomTransformation { transformation: TransformationInternal, block: Option<Action>},
-    ForEachTransformation { transformation: TransformationInternal, block: Option<Action>},
-    RotatedBy { number: Number, block: Option<Action> },
-    If { condition: Option<Conditions>, result: Option<Action>, r#else: Option<Action>}, // If block, if true, execute action
+    RandomTransformation { transformation: TransformationInternal, block: Option<Vec<Action>>},
+    ForEachTransformation { transformation: TransformationInternal, block: Option<Vec<Action>>},
+    RotatedBy { number: Number, block: Option<Vec<Action>> },
+    If { condition: Option<Conditions>, result: Option<Vec<Action>>, r#else: Option<Vec<Action>>}, // If block, if true, execute action
     IncreaseParticlePropierty { propierty: ParticlePropierties, number: Number,  direction: Direction },
     SetParticlePropierty { propierty: ParticlePropierties, number: Number, direction: Direction },
-    Repeat { number: Number, block: Option<Action> },
-    EveryXFrames { number: Number, block: Option<Action> },
+    Repeat { number: Number, block: Option<Vec<Action>> },
+    EveryXFrames { number: Number, block: Option<Vec<Action>> },
     None
 }
 
@@ -89,7 +89,7 @@ impl Actions {
                 }
 
                 let block = block.unwrap();
-                let func = block.to_func(api);
+                let func = block.iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
 
                 Box::new(move |plugin, particle, api| {
                     let previous_trasnformation = api.get_transformation().clone();
@@ -97,7 +97,7 @@ impl Actions {
                     let transformation = transformation.to_transformation(api);
                     api.set_transformation(transformation);
 
-                    func(plugin, particle, api);
+                    func.iter().for_each(|func| func(plugin, particle, api));
 
                     api.set_transformation(previous_trasnformation);
                 })
@@ -113,7 +113,7 @@ impl Actions {
 
                 let block = block.unwrap();
 
-                let func = block.to_func(api);
+                let func = block.iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
 
                 match transformation {
                     TransformationInternal::HorizontalReflection => {
@@ -123,12 +123,12 @@ impl Actions {
                             let transformation = Transformation::HorizontalReflection(true);
                             api.set_transformation(transformation);
 
-                            func(plugin, particle, api);
+                            func.iter().for_each(|func| func(plugin, particle, api));
 
                             let transformation = Transformation::HorizontalReflection(false);
                             api.set_transformation(transformation);
 
-                            func(plugin, particle, api);
+                            func.iter().for_each(|func| func(plugin, particle, api));
 
                             api.set_transformation(previous_trasnformation);
                         })
@@ -140,12 +140,12 @@ impl Actions {
                             let transformation = Transformation::VerticalReflection(true);
                             api.set_transformation(transformation);
 
-                            func(plugin, particle, api);
+                            func.iter().for_each(|func| func(plugin, particle, api));
 
                             let transformation = Transformation::VerticalReflection(false);
                             api.set_transformation(transformation);
 
-                            func(plugin, particle, api);
+                            func.iter().for_each(|func| func(plugin, particle, api));
 
                             api.set_transformation(previous_trasnformation);
                         })
@@ -156,22 +156,22 @@ impl Actions {
                         let transformation = Transformation::Reflection(true, true);
                         api.set_transformation(transformation);
 
-                        func(plugin, particle, api);
+                        func.iter().for_each(|func| func(plugin, particle, api));
 
                         let transformation = Transformation::Reflection(false, false);
                         api.set_transformation(transformation);
 
-                        func(plugin, particle, api);
+                        func.iter().for_each(|func| func(plugin, particle, api));
 
                         let transformation = Transformation::Reflection(false, true);
                         api.set_transformation(transformation);
 
-                        func(plugin, particle, api);
+                        func.iter().for_each(|func| func(plugin, particle, api));
 
                         let transformation = Transformation::Reflection(true, false);
                         api.set_transformation(transformation);
 
-                        func(plugin, particle, api);
+                        func.iter().for_each(|func| func(plugin, particle, api));
 
                         api.set_transformation(previous_trasnformation);
                     }),
@@ -182,13 +182,13 @@ impl Actions {
                             let transformation = Transformation::Rotation(i);
                             api.set_transformation(transformation);
 
-                            func(plugin, particle, api);
+                            func.iter().for_each(|func| func(plugin, particle, api));
                         }
 
                         api.set_transformation(previous_transformation);
                     }),
                     TransformationInternal::None => {
-                        Box::new(move |plugin, particle, api| func(plugin, particle, api))
+                        Box::new(move |plugin, particle, api| func.iter().for_each(|func| func(plugin, particle, api)))
                     }
                 }
             }
@@ -203,26 +203,26 @@ impl Actions {
                 }
 
                 let condition = condition.unwrap().to_func(api);
-                let result = result.unwrap_or(Box::new(Actions::None)).to_func(api);
+                let result = result.unwrap_or(vec![Box::new(Actions::None)]).iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
                 // "Baking" so we only call r#else if there is an else
                 // I could also create an empty function with let r#else = r#else.unwrap_or_else(|| Box::new(|_, _, _| true));
                 // And I could return a single lamnbda instead of this but....
                 // These funcs might get called a lot, this is hotpath so I prefer to optimize as much as possible
                 match r#else {
                     Some(r#else) => {
-                        let r#else = r#else.to_func(api);
+                        let r#else = r#else.iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
                         Box::new(move |plugin, particle, api| {
                             if condition(plugin, particle, api) {
-                                result(plugin, particle, api)
+                                result.iter().for_each(|func| func(plugin, particle, api));
                             } else {
-                                r#else(plugin, particle, api)
+                                r#else.iter().for_each(|func| func(plugin, particle, api));
                             };
                         })
                     }
                     // By default, an if blocks returns true
                     None => Box::new(move |plugin, particle, api| {
                         if condition(plugin, particle, api) {
-                            result(plugin, particle, api);
+                            result.iter().for_each(|func| func(plugin, particle, api));
                         }
                     }),
                 }
@@ -233,7 +233,7 @@ impl Actions {
                 }
 
                 let block = block.unwrap();
-                let func = block.to_func(api);
+                let func = block.iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
                 Box::new(move |plugin, particle, api| {
                     let previous_transformation = api.get_transformation().clone();
                     let rotations = number.to_number(api);
@@ -241,7 +241,7 @@ impl Actions {
                     let rotations = rotations.rem_euclid(8);
                     let transformation = Transformation::Rotation(rotations as usize);
                     api.set_transformation(transformation);
-                    func(plugin, particle, api);
+                    func.iter().for_each(|func| func(plugin, particle, api));
                     api.set_transformation(previous_transformation);
                 })
             }
@@ -297,12 +297,12 @@ impl Actions {
                 }
 
                 let block = block.unwrap();
-                let func = block.to_func(api);
+                let func = block.iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
 
                 Box::new(move |plugin, particle, api| {
                     let times = number.to_number(api);
                     for _ in 0..times {
-                        func(plugin, particle, api);
+                        func.iter().for_each(|func| func(plugin, particle, api));
                     }
                 })
             }
@@ -312,12 +312,12 @@ impl Actions {
                 }
 
                 let block = block.unwrap();
-                let func = block.to_func(api);
+                let func = block.iter().map(|block| block.to_func(api)).collect::<Vec<_>>();
 
                 Box::new(move |plugin, particle, api| {
                     let frames = number.to_number(api) as u32;
                     if api.get_frame() % frames == 0 {
-                        func(plugin, particle, api);
+                        func.iter().for_each(|func| func(plugin, particle, api));
                     }
                 })
             }
